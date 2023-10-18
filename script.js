51bfccead597a4e4bd7598d04f53677dbe06ad4c
Diego Cornejo <cornejodl13@icloud.com>
2023-10-18 18:53:07 -0500

    still need work

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var time = dayjs();
+var formatTime = time.format('dddd, MMMM D');
+
+$("#currentDay").text(formatTime);
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -9,6 +14,8 @@ $(function runProgram () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  var 
 
   allbtns.addEventListener('click', function(event)
   {
