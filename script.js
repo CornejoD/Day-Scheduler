4c302ea2993751ecb3dd59400a23e24259d7716c
Diego <cornejodl13@gmail.com>
2023-10-24 11:33:18 -0500

    Working on dayjs, Mac

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-var time = dayjs();
-var formatTime = time.format('dddd, MMMM D');
+$(function ()
+{
+  var time = dayjs();
+  var formatTime = time.format('MMMM D, YYYY h:m');
 
-$("#currentDay").text(formatTime);
+  $("#currentDay").text(formatTime);
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var allbtns = document.querySelectorAll(".saveBtn");
-$(function runProgram () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+    // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+    // the code isn't run until the browser has finished rendering all the elements
+    // in the html.
+    var allbtns = document.querySelectorAll(".saveBtn");
 
-  for (let i = 0; i < allbtns.length; i++)
+    // TODO: Add a listener for click events on the save button. This code should
+    // use the id in the containing time-block as a key to save the user input in
+    // local storage. HINT: What does `this` reference in the click listener
+    // function? How can DOM traversal be used to get the "hour-x" id of the
+    // time-block containing the button that was clicked? How might the id be
+    // useful when saving the description in local storage?
+  $(".time-block").each(function()
   {
-    allbtns[i].addEventListener('click', function(){
-      console.log("CLICKED", this.textContent);
-      console.log('Button index:', i);
-    })
-  }
+
+ 
+    var hour = parseInt($(this).attr("id"));
+    let currHour = time.hour();
+
+    console.log(currHour);
+
+    $(this).removeClass().addClass("row time-block");
+
+    if(hour == currHour)
+    {
+      $(this).addClass("present");
+    }
+    else if(hour < currHour)
+    {
+      $(this).addClass("past");
+    }
+    else if(hour > currHour)
+    {
+      $(this).addClass("future");
+    }
+  });
+
+
+    for (let i = 0; i < allbtns.length; i++)
+    {
+      allbtns[i].addEventListener('click', function(){
+        console.log("CLICKED", this.textContent);
+        console.log('Button index:', i);
+      })
+    }
+});
+
+
 
   
 
@@ -38,7 +68,7 @@ $(function runProgram () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
 
 // $(document).ready(function()
 // {
